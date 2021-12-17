70079b4e8bd2bec072cb599674db8fa684afc3b4
Elysu <xnicholasmy@gmail.com>
2021-12-18 01:27:12 +0800

    edit_class in progress

--- lib/screens/class/edit_class.dart
+++ lib/screens/class/edit_class.dart
@@ -20,15 +20,11 @@ class _EditClassState extends State<EditClass> {
   Map classTeacher = {};
   bool loading = true;
   int? attendance;
-  Timestamp? tStart;
-  Timestamp? tEnd;
-  DateTime? dStart;
-  DateTime? dEnd;
-  DateTime? newClassDate;
-  String date = "";
-  String classStatus = "";
+  Timestamp? tStart, tEnd; // to store timestamp from firestore
+  DateTime? dStart, dEnd, newStartDateTime, newEndDateTime; // to convert time stamp to DateTime and store new selected date
+  String strDate = "", strStartTime = "", strEndTime = "", classStatus = "", classroom = ""; // String that display on form
+  TimeOfDay? startTime, endTime, newStartTime, newEndTime;
   List<String> allClassroom = [];
-  String? dropdownValue;
 
   @override
   void initState() {
@@ -40,13 +36,22 @@ class _EditClassState extends State<EditClass> {
         loading = false;
         tStart = classDetails['c_datetimeStart'];
         tEnd = classDetails['c_datetimeEnd'];
+
+        // set date into variables
         dStart = tStart!.toDate();
         dEnd = tEnd!.toDate();
-        date = "${DateFormat('d').format(dStart!).toString()}/${DateFormat('yM').format(dStart!).toString()}";
+        newStartDateTime = dStart;
+        strDate = "${DateFormat('d').format(newStartDateTime!).toString()}/${DateFormat('yM').format(newStartDateTime!).toString()}";
+        startTime = TimeOfDay(hour: dStart!.hour, minute: dStart!.minute);
+        endTime = TimeOfDay(hour: dEnd!.hour, minute: dEnd!.minute);
+        strStartTime = DateFormat('jm').format(dStart!).toString();
+        strEndTime = DateFormat('jm').format(dEnd!).toString();
         classStatus = classDetails['c_ongoing'] ? "Ongoing" : "Not Available";
+        classroom = classDetails['classroom'];
       });
     });
 
+    // loop all classroom to dropdown list
     String strClassroom = "A";
     for (int i=1; i<=50; i++) {
       allClassroom.add("${strClassroom}${i}");
@@ -123,7 +128,7 @@ class _EditClassState extends State<EditClass> {
                             const Text("Date:"),
                             const SizedBox(height: 5),
                             Text(
-                              date,
+                              strDate,
                               style: const TextStyle(fontSize: 20),
                             ),
                           ],
@@ -137,7 +142,7 @@ class _EditClassState extends State<EditClass> {
                               width: double.infinity,
                               child: ElevatedButton.icon(
                                 onPressed: () {
-                                  pickDate(context, dStart!);
+                                  pickDate(context);
                                 },
                                 icon: const Icon(Icons.date_range),
                                 label: const Text("PICK A DATE")),
@@ -161,7 +166,7 @@ class _EditClassState extends State<EditClass> {
                             const Text("Start Time:"),
                             const SizedBox(height: 5),
                             Text(
-                              DateFormat('jm').format(dStart!).toString(),
+                              strStartTime,
                               style: const TextStyle(fontSize: 20),
                             ),
                           ],
@@ -174,7 +179,7 @@ class _EditClassState extends State<EditClass> {
                           width: double.infinity,
                           child: ElevatedButton.icon(
                             onPressed: () {
-                              pickTime(context);
+                              pickTime(context, 1);
                             },
                             icon: const Icon(Icons.access_time),
                             label: const Text("PICK A TIME")
@@ -196,7 +201,7 @@ class _EditClassState extends State<EditClass> {
                             const Text("End Time:"),
                             const SizedBox(height: 5),
                             Text(
-                              DateFormat('jm').format(dEnd!).toString(),
+                              strEndTime,
                               style: const TextStyle(fontSize: 20),
                             )
                           ],
@@ -209,7 +214,7 @@ class _EditClassState extends State<EditClass> {
                           width: double.infinity,
                           child: ElevatedButton.icon(
                             onPressed: () {
-
+                              pickTime(context, 2);
                             },
                             icon: const Icon(Icons.share_arrival_time_outlined),
                             label: const Text("PICK A TIME")
@@ -234,7 +239,7 @@ class _EditClassState extends State<EditClass> {
                               decoration: const InputDecoration(
                                 label: Text("Select Classroom")
                               ),
-                              value: dropdownValue,
+                              value: classroom,
                               items: allClassroom.map<DropdownMenuItem<String>>((String val) {
                                 return DropdownMenuItem<String>(
                                   value: val,
@@ -243,7 +248,7 @@ class _EditClassState extends State<EditClass> {
                               }).toList(),
                               onChanged: (String? newValue) {
                                 setState(() {
-                                  dropdownValue = newValue!;
+                                  classroom = newValue!;
                                 });
                               },
                             )
@@ -295,7 +300,9 @@ class _EditClassState extends State<EditClass> {
                     width: double.infinity,
                     child: ElevatedButton(
                       onPressed: () {
+                        newStartDateTime
 
+                        print(newStartDateTime);
                       },
                       child: const Text("Save"),
                     ),
@@ -307,10 +314,11 @@ class _EditClassState extends State<EditClass> {
     );
   }
 
-  Future pickDate(BuildContext context, DateTime dStart) async {
+  // functions to show date and time picker
+  Future pickDate(BuildContext context) async {
     final newDate = await showDatePicker(
       context: context,
-      initialDate: newClassDate ?? dStart,
+      initialDate: newStartDateTime ?? DateTime.now(),
       firstDate: DateTime(DateTime.now().year - 5),
       lastDate: DateTime(DateTime.now().year + 5),
     );
@@ -318,12 +326,51 @@ class _EditClassState extends State<EditClass> {
     if (newDate == null) return;
 
     setState(() {
-      newClassDate = newDate;
-      date = "${DateFormat('d').format(newClassDate!).toString()}/${DateFormat('yM').format(newClassDate!).toString()}";
+      newStartDateTime = newDate;
+      strDate = "${DateFormat('d').format(newStartDateTime!).toString()}/${DateFormat('yM').format(newStartDateTime!).toString()}";
     });
   }
 
-  Future pickTime(BuildContext context) async {
+  // 1 = start time , 2 = end time
+  Future pickTime(BuildContext context, int type) async {
+    final initialTime = TimeOfDay(hour: DateTime.now().hour, minute: DateTime.now().minute);
+    TimeOfDay? newTime;
+    var dateFormat = DateFormat("h:mm a"); // convert to 12 hours format
 
+    switch (type) {
+      case 1: {
+        newTime = await showTimePicker(
+          context: context,
+          initialTime: newStartTime ?? startTime!
+        );
+
+        if (newTime == null) return;
+
+        setState(() {
+          newStartTime = newTime;
+          //newStartDateTime.hour(newStartTime.hour);
+          DateTime tempDate = DateFormat("hh:mm").parse(newStartTime!.hour.toString() + ":" + newStartTime!.minute.toString());
+          strStartTime = dateFormat.format(tempDate);
+        });
+        break;
+      }
+      case 2: {
+        newTime = await showTimePicker(
+          context: context,
+          initialTime: newEndTime ?? endTime!
+        );
+
+        if (newTime == null) return;
+
+        setState(() {
+          newEndTime = newTime;
+          DateTime tempDate = DateFormat("hh:mm").parse(newEndTime!.hour.toString() + ":" + newEndTime!.minute.toString());
+          strEndTime = dateFormat.format(tempDate);
+        });
+        break;
+      }
+    }
+
+    if (newTime == null) return;
   }
 }
